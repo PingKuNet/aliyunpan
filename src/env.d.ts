24850f159ad7902032c756212123933f954d0ebe
PingKu <48786066+PingKuNet@users.noreply.github.com>
2025-07-11 17:43:44 +0800

    Update env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 /// <reference types="vite/client" />
 
+// vue 相关配置
 declare module '*.vue' {
   import { DefineComponent } from 'vue'
   // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
